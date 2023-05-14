93db689dbe9140e07aec66f3139688a119b584ce
Fantomlance <125268565+Fantomlance@users.noreply.github.com>
2023-05-14 23:09:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,32 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0;
+  int right = size - 1;
+  int middle = 0;
+  int count = 0;
+  while (left <= right) {
+    middle = (left + right) / 2;
+    if (*(arr + middle) > value) {
+      right = middle - 1;
+    }
+    if (*(arr + middle) < value) {
+      left = middle + 1;
+    }
+    if (*(arr + middle) == value) {
+            count++;
+            int i = middle - 1;
+            while (i >= left && arr[i] == value) {
+                count++;
+                i--;
+            }
+            int j = middle + 1;
+            while (j <= right && arr[j] == value) {
+                count++;
+                j++;
+            }
+            return count;
+    }
+  }
+  return 0;
 }
